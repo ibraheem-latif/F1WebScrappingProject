943dbdc86aa4a580ea2677c8b6f84457096b15b2
ibraheem-latif <ibraheem_latif@hotmail.com>
2023-05-24 14:36:05 +0100

    displays json on browser

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import json
 from flask import Flask
+from f1_scrapper import scrape_data
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, World!"
+
+@app.route('/scrape', methods=['GET'])
+def scrape():
+    data = scrape_data()
+    return data.to_json(orient='records')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
